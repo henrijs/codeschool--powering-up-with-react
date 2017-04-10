ead427bcef96c1e91bb423f635732172293eab30
Henrijs Seso <henrijs.seso@gmail.com>
2017-04-10 16:22:38 +0300

    Make agnostic, add demo.

---
+++ components.js
@@ -0,0 +1,9 @@
+class Demo extends React.Component {
+  render() {
+    return(<div className="demo">Demo of super simple React setup.</div>);
+  }
+}
+
+ReactDOM.render(
+<Demo />, document.getElementById('demo')
+);
